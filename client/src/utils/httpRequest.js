442fa03d0e906abd43966c7d7c9ce47e949e3d4b
vientran199 <vientran1999@gmail.com>
2022-06-14 14:29:33 +0700

    test deploy

--- client/src/utils/httpRequest.js
+++ client/src/utils/httpRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { LOCAL_STORAGE_TOKEN_NAME } from '~/contexts/Constans';
 
 const defaultOptions = {
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json',
     },
